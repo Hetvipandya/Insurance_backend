9559739db237e7398203a8040b472df9ab4c9a8e
Holly Zolly <test@example.com>
2026-05-15 15:36:39 +0530

    Initial commit

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,28 +1,24 @@
 const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
 
-// ================= CREATE UPLOAD FOLDER =================
-const uploadPath = "uploads/";
+// ================= CLOUDINARY CONFIG =================
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true });
-}
-
-// ================= STORAGE ================= 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadPath);
-  },
-
-  filename: function (req, file, cb) {
-    const uniqueName =
-      Date.now() + "-" + Math.round(Math.random() * 1e9);
-
-    cb(
-      null,
-      uniqueName + path.extname(file.originalname)
-    );
+// ================= STORAGE =================
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: "insurance",
+      resource_type: "auto",
+      public_id:
+        Date.now() + "-" + file.originalname.split(".")[0],
+    };
   },
 });
 
