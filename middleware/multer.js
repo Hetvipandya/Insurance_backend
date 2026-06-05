e41cd290533497fe46ae6f4d2e8a682898cc3ee0
Holly Zolly <test@example.com>
2026-06-05 12:08:04 +0530

    Initial commit

--- middleware/multer.js
+++ middleware/multer.js
@@ -135,9 +135,9 @@ const fileFilter = (
 const upload = multer({
   storage,
   fileFilter,
-  limits: {
-    fileSize: 50 * 1024 * 1024,
-  },
+  // limits: {
+  //   fileSize: 50 * 1024 * 1024,
+  // },
 });
 
 module.exports =
